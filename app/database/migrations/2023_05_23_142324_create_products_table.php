2be6a11a617492867e3e95ce93e84c4d10895308
hidetoshi3209 <hidetoshi.3209@gmail.com>
2023-05-23 23:45:27 +0900

    20230523-出品登録-実装中

---
+++ app/database/migrations/2023_05_23_142324_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title','50');
+            $table->integer('price','11');
+            $table->string('comment','255');
+            $table->integer('condition','10');
+            $table->string('image','255');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
