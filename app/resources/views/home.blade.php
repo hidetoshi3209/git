8deda0e0a682eb9d3a0fdfe89ab56705fb1ee3e3
hidetoshi3209 <hidetoshi.3209@gmail.com>
2023-05-30 02:17:58 +0900

    20230529 - メインページ表示機能 - 実装中

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -1,26 +1,41 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>自作製造</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
-</head>
-<body>
-    <h1>てすと</h1>
-</body>
-</html>
+@extends('layouts.layout')
+@section('content')
+    <main>
+    <div class="row justify-content-center">
+                <div class="col-6 mb-4">
+                    <div>
+                        <form>
+                        @csrf
+                            <div class="bd-highlight my-3 mx-3">
+                                <div class="my-3">
+                                    <input class="form-control me-2" type="search" placeholder="何をおさがしですか？" aria-label="Search">
+                                </div>
+                                <div class="d-flex justify-content-center flex-row bd-highlight">
+                                    <select>
+                                        <option value="100">100円以上</option>
+                                        <option value="500">500円以上</option>
+                                        <option value="1000">1000円以上</option>
+                                        <option value="3000">3000円以上</option>
+                                        <option value="5000">5000円以上</option>
+                                        <option value="10000">10000円以上</option>
+                                    </select>
+                                    <span>~</span>
+                                    <select>
+                                        <option value="100">100円以下</option>
+                                        <option value="500">500円以下</option>
+                                        <option value="1000">1000円以下</option>
+                                        <option value="3000">3000円以下</option>
+                                        <option value="5000">5000円以下</option>
+                                        <option value="10000">10000円以下</option>
+                                    </select>
+                                </div>
+                                <div class="d-flex justify-content-center">   
+                                    <button type="submit" class="btn btn-info text-white mt-3">検索</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </main>
+@endsection
---
+++ app/resources/views/layouts/layout.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', '自作製造') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @yield('stylesheet')
+</head>
+<body>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">メルカリ</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">ログイン</a>
+                        </li>
+                        <span>/</span>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">新規会員登録</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    @yield('content')
+</body>
+</html>
